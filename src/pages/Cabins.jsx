407c978b17dbb91fa6fc786cbb60361b8ad65c7a
Mohamed Abassia <mo3bassia@gmail.com>
2025-05-11 21:09:11 +0300

    fetch cabins data from supabase table and handling it with react-query

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,20 +2,20 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import { useQuery } from "@tanstack/react-query";
+import CabineTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    async function fetchCabins() {
-      const data = await getCabins();
-      console.log(data);
-    }
-    fetchCabins();
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabineTable />
+      </Row>
+    </>
   );
 }
 
