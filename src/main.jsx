1293e0a96c7dd03df2c5cca77189d0cfbdbf6a51
Mohamed Abassia <mo3bassia@gmail.com>
2025-05-11 20:41:17 +0300

    install react-query library to manage remote state

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
