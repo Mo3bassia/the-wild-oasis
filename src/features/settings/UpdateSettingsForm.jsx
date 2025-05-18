6a4e463e56d745ab2125b1c9299ff961034b2dc6
Mohamed Abassia <mo3bassia@gmail.com>
2025-05-19 00:30:12 +0300

    handle editing the settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,37 +1,68 @@
+import { useEffect } from "react";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useEditSettings } from "./useEditSettings";
 import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
-  const { data: settings, isLoading } = useSettings();
+  const { data: settings, isLoading: isFetching } = useSettings();
   const {
     minBookingLength,
     maxBookingLength,
     maxGuestsPerBooking,
     breakfastPrice,
   } = settings || {};
-  console.log(settings);
+
+  const { mutate: updateSettings, isLoading: isUpdating } = useEditSettings();
+
+  function handleUpdateSettings(name, value) {
+    updateSettings({ [name]: value });
+  }
+
+  const isWorking = isFetching || isUpdating;
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          disabled={isWorking}
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          onBlur={(e) =>
+            handleUpdateSettings("minBookingLength", e.target.value)
+          }
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          disabled={isWorking}
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) =>
+            handleUpdateSettings("maxBookingLength", e.target.value)
+          }
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
+          disabled={isWorking}
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) =>
+            handleUpdateSettings("maxGuestsPerBooking", e.target.value)
+          }
         />
       </FormRow>
       <FormRow label="Breakfast price">
         <Input
+          disabled={isWorking}
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdateSettings("breakfastPrice", e.target.value)}
         />
       </FormRow>
     </Form>
---
+++ src/features/settings/useEditSettings.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting } from "../../services/apiSettings";
+import toast from "react-hot-toast";
+
+export function useEditSettings() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data) => {
+      return updateSetting(data);
+    },
+    onSuccess: () => {
+      toast.success("Settings have been updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+      queryClient.refetchQueries({ queryKey: ["settings"] });
+    },
+    onError: (error) => {
+      toast.error("Error updating settings");
+      console.error(error);
+    },
+  });
+}
